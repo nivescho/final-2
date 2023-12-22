42a0b07413526c91e2512be4d54c86998e943dfd
nivescho <152449888+nivescho@users.noreply.github.com>
2023-12-22 18:04:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+function getID(id) {
+    return document.getElementById(id).value; 
+}
+function disableInput(id) {
+    document.getElementById(id).disabled = true;
+}
+
+
+
+function innerHTML(id,result){
+    return document.getElementById(id).innerHTML=result;
+}
+
+function contadorCaracteres() {
+    var mensajeInput = document.getElementById("mensaje");
+    var textVista = document.getElementById("textvista");
+
+    mensajeInput.addEventListener("input", function() {
+        var mensajeValue = this.value; // Obtener el valor del campo de entrada
+        var mensajeLength = mensajeValue.length; // Obtener la longitud del mensaje
+
+        if (mensajeLength > 1000) {
+            textVista.innerHTML = "Máximo de caracteres excedido";
+            mensajeInput.disabled = true; // Deshabilitar la entrada si se excede el límite
+        } else {
+            textVista.innerHTML = mensajeLength; // Mostrar el recuento actual de caracteres
+            mensajeInput.disabled = false; // Habilitar la entrada si está dentro del límite
+        }
+    });
+}
+
+
+
+// window.addEventListener("load",inicio,true);
+
+
+document.getElementById("cifrar").addEventListener("click",function() {
+    let texto = document.getElementById("mensaje").value;
+    let ingresa = document.getElementById("offset").value;
+        
+    console.log(texto);
+    console.log(ingresa);
+    document.getElementById("mensaje2").value=cifrar(texto,ingresa);
+});
+
+document.getElementById("decifrar").addEventListener("click",function() {
+    let texto = document.getElementById("mensaje").value;
+    let ingresa = document.getElementById("offset").value;
+    document.getElementById("mensaje2").value = decifrar(texto,ingresa);
+});
+
+function cifrar (texto, ingresa){
+    let resultado="";
+    let letras ="abcdefghijklmnñopqrstuvwxyz";
+
+    ingresa = ( ingresa % 26+26) %  26;
+
+    if (texto){
+        for (let i = 0; i <texto.length; i++){
+            if (letras.indexOf(texto[i])!=-1){
+                let posicion =((letras.indexOf(texto[i])+ingresa)%26);
+                resultado +=letras[posicion];
+            }
+            else
+                resultado += texto[i];
+                
+        }
+    }
+   return resultado;
+}
+
+function decifrar (texto, ingresa){
+    let resultado="";
+    let letras ="abcdefghijklmnñopqrstuvwxyz";
+
+    ingresa = ( ingresa % 26-26) %  26;
+
+    if (texto){
+        for (let i = 0; i <texto.length; i++){
+            if (letras.indexOf(texto[i])!=-1){
+                let posicion =((letras.indexOf(texto[i])-ingresa)%26);
+                resultado +=letras[posicion];
+            }
+            else
+                resultado += texto[i];
+                
+        }
+    }
+    return resultado;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
